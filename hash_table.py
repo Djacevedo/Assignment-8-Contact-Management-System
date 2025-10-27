7d6770ea84cc57ccf68866fa3836a78209ccfd76
Djacevedo <djacevedo05@gmail.com>
2025-10-27 02:11:26 +0000

    Hash table

--- hash_table.py
+++ hash_table.py
@@ -6,7 +6,12 @@ class Contact:
         number (str): The phone number of the contact.
     '''
     
-    pass # Delete this line when implementing the class
+    def __init__(self,name,number):
+        self.name=name
+        self.number=number
+
+    def __repr__(self):
+        return f"{self.name}:{self.number}")
 
 class Node:
     '''
@@ -17,7 +22,10 @@ class Node:
         next (Node): Pointer to the next node in case of a collision.
     '''
    
-    pass # Delete this line when implementing the class
+    def __init__(self,key,value):
+        self.key=key
+        self.value=value
+        self.next=None
 
 class HashTable:
     '''
@@ -32,6 +40,65 @@ class HashTable:
         print_table(): Prints the structure of the hash table.
     '''
     
-    pass # Delete this line when implementing the class
+    def __init__(self,size=10):
+        self.size=size
+        self.data=[None]*self.size
+
+    def hash_function(self,key):
+        hash_sum=0
+        for i, char in enumerate(key):
+            hash_sum+=(i+1)*ord(char)
+        return hash_sum % self.size
+
+    def insert(self,value,key):
+        index=self.hash_function(key)
+        new_node=Node(key,value)
+        if self.data[index] is None:
+            self.data[index]=new_node
+            return
+        current=self.data[index]
+        while True:
+            if current.key==key:
+                current.value=value
+                return
+            if current.next is None:
+                break
+            current=current.next
+        current.next=new_node
+    
+    def search(self,key):
+        index=self.hash_function(key)
+        current=self.data[index]
+        while current:
+            if current.key==key:
+                return current.value
+            current=current.next
+        return None
+
+    def print_table(self):
+        for i, node in enumerate(self.data):
+            print(f"Index {i}:",end=" ")
+            current=Node
+            if not current:
+                print("empty")
+            else:
+                while current:
+                    print(f"[{current.key}:{current.value.number}]", end=" -> ")
+                    current=current.next
+                print('None')
 
 # Test your hash table implementation here.  
+if __name__ == "__main__":
+    table =HashTable(size=10)
+    table.insert("Alice",Contact("Alice","555-1234"))
+    table.insert("Bob",Contact("Bob","555-5678"))
+    table.insert("Charlie",Contact("Charlie","555-8765"))
+    table.insert("Alicia",Contact("Alicia","555-9999"))
+
+    print("\n--- Contact Lookup ---")    
+    print(table.search("Alice"))    
+    print(table.search("Bob"))    
+    print(table.search("Eve"))
+
+    print("\n--- Hash Table Structure ---")
+    table.print_table()
